4a2079c17ea75bb60b3c94e1394a9b00c71053fb
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-10 15:28:43 -0500

    finished vid 9

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+
 class App extends React.Component {
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh seafood market" />
         </div>
         <Order />
         <Inventory />
