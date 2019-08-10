59ae44c7bee641e6737cdfd5dfbc148aa9f3510b
Brandon Bunch <brandon_bunch@icloud.com>
2019-08-10 11:03:57 -0500

    Finished episodes 1-6

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <>
+    <form className="store-selector">
+      <h2>Please Enter A Store</h2>
+      <input type="text" requered placeholder="Store Name" />
+      <button type="submit">Visit Store &rarr;</button>
+    </form>
+    </>
+    )
+  }
+}
+
+export default StorePicker;
