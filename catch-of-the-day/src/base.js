8d34e49fcc5ca32b2452c0793b2fbb47a620ff41
Brandon Bunch <brandon_bunch@icloud.com>
2019-10-18 09:33:26 -0500

    Update base.js

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCZx50uUGEsnve5MQ1yWdWH9by0RgpHPEM",
+  apiKey: "",
   authDomain: "catch-of-the-day-brandon-bunch.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-brandon-bunch.firebaseio.com"
 });
